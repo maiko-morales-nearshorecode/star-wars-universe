fa5e83f0259f5dee1088ddf420d4db2f9c7dd144
Maiko Morales <maiko.morales@nearshorecode.com>
2019-11-01 12:04:37 -0400

    Setting up redux store

---
+++ src/reducers/characters.js
@@ -0,0 +1,13 @@
+const initialState = {
+  items: [],
+  isLoading: false
+};
+
+const characters = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+export default characters;
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+import characters from './characters';
+
+const rootReducer = combineReducers({ characters });
+
+export default rootReducer;

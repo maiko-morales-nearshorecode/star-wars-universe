bbb1e67fa4ea1071d5c94bfeb3445924ffe8c28d
Maiko Morales <maiko.morales@nearshorecode.com>
2019-11-01 15:09:28 -0400

    Extracting Character details out of Characters list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import Characters from './components/Characters/Characters';
+import CharacterDetails from './components/CharacterDetails/CharacterDetails';
 
 function App() {
   return (
-    <div>
+    <Grid container columns={2}>
       <Characters />
-    </div>
+      <CharacterDetails />
+    </Grid>
   );
 }
 
--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Grid, List, Icon, Pagination } from 'semantic-ui-react';
 import { fetchCharacters } from '../../actions/characters';
 import Character from './Character';
-import CharacterDetails from '../CharacterDetails/CharacterDetails';
 
 const Characters = () => {
   const characters = useSelector(state => state.characters);
@@ -18,31 +17,24 @@ const Characters = () => {
   };
 
   return (
-    <>
-      <Grid container columns={2}>
-        <Grid.Column>
-        <h1>Characters</h1>
-          {characters.isLoading && <Icon name="spinner" size="large" loading />}
-          {!characters.isLoading && characters.items.length > 0 &&
-            <>
-              <List divided relaxed selection verticalAlign="middle">
-                {characters.items.map(character => (
-                  <Character key={character.url} {...character} />
-                ))}
-              </List>
-              <Pagination
-                activePage={characters.page}
-                totalPages={characters.totalPages}
-                onPageChange={handlePageChange}
-              />
-            </>
-          }
-        </Grid.Column>
-        <Grid.Column>
-          <CharacterDetails />
-        </Grid.Column>
-      </Grid>
-    </>
+    <Grid.Column>
+      <h1>Characters</h1>
+      {characters.isLoading && <Icon name="spinner" size="large" loading />}
+      {!characters.isLoading && characters.items.length > 0 &&
+        <>
+          <List divided relaxed selection verticalAlign="middle">
+            {characters.items.map(character => (
+              <Character key={character.url} {...character} />
+            ))}
+          </List>
+          <Pagination
+            activePage={characters.page}
+            totalPages={characters.totalPages}
+            onPageChange={handlePageChange}
+          />
+        </>
+      }
+    </Grid.Column>
   );
 };
 
